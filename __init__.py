944f474cde4caa9efcc56e93488019475ca8c381
Bering <bering@ringlogic.com>
2019-01-07 21:52:52 -0500

    Planet types and sizes

--- __init__.py
+++ __init__.py
@@ -10,4 +10,4 @@ print("Created world with " + str(len(world.stars)) + " stars")
 for s in world.stars:
 	print("* " + s.name)
 	for p in s.planets:
-		print("  o " + p.name)
+		print("  o " + p.name + " (" + p.size + " " + p.type + ")")
--- planet.py
+++ planet.py
@@ -1,7 +1,13 @@
+import random
+
+sizes = ["Tiny", "Small", "Medium", "Large", "Huge"]
+types = ["Baren", "Arid", "Terran", "Rich", "Gaïa"]
 suffixes = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa", "Lambda", "Mu", "Nu", "Xi", "Omicron", "Pi", "Rho", "Sigma", "Tau", "Upsilon", "Phi", "Chi", "Psi", "Omega"]
 
 class Planet:
 
 	def __init__(self, star):
 		self.star = star
+		self.size = sizes[random.randrange(0,len(sizes))]
+		self.type = types[random.randrange(0,len(types))]
 		self.name = star.name + " " + suffixes[len(star.planets)]
