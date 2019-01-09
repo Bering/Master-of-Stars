162664fb73cf64b93e013a02422cd617d7d05363
Bering <bering@ringlogic.com>
2019-01-08 20:10:26 -0500

    rename + use IndexError instead of RuntimeError

--- star_names_stack.py
+++ star_names_stack.py
@@ -1,18 +1,19 @@
 import random
 
-class StarNamesList:
+class StarNamesStack:
 
 	def __init__(self):
 		f = open("star_names.txt", "r")
 		self.names = f.readlines()
+		f.close()
+		
 		for item in enumerate(self.names):
 			index = item[0]
 			name = item[1]
 			self.names[index] = name.strip()
-		f.close()
 
 	def pop(self):
-		if (len(self.names) == 0): raise RuntimeError("Out of star names!")
+		if (not self.names): raise IndexError("Out of star names!")
 
 		index = random.randrange(0, len(self.names))
 		name = self.names[index]
--- world.py
+++ world.py
@@ -1,15 +1,15 @@
 import random
 from star import Star
-from star_names_list import StarNamesList
+from star_names_stack import StarNamesStack
 
 class World:
 
-	starNamesList = StarNamesList()
+	star_names = StarNamesStack()
 
 	def __init__(self, config):
 		self.stars = []
 		for n in range(0, config.nb_stars):
-			star = Star(self.starNamesList.pop())
+			star = Star(self.star_names.pop())
 			self.stars.append(star)
 
 			nb_planets = random.randrange(config.min_planets_per_star, config.max_planets_per_star+1)
