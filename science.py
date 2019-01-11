a375c730e3ca8efac21fc604e2bbe5e9fc8f2b87
Bering <bering@ringlogic.com>
2019-01-10 19:02:59 -0500

    Research progress resets upon research effect

--- science.py
+++ science.py
@@ -8,15 +8,11 @@ class ResearchBase:
 	def next_turn(self, planet):
 		self.progress += planet.science
 
-		item_count = 0
+		if(self.progress >= self.cost):
+			self.progress = 0
+			self.effect(planet)
 
-		while(self.progress > self.cost):
-			self.progress -= self.cost
-			item_count ++
-
-		self.effect(planet, item_count)
-
-	def effect(self, planet, item_count):
+	def effect(self, planet):
 		pass
 
 class ResearchShipyard(ResearchBase):
@@ -24,13 +20,13 @@ class ResearchShipyard(ResearchBase):
 	def __init__(self, planet):
 		super().__init__("Upgrade Shipyard", 50 * planet.shipyard_level)
 
-	def effect(self, planet, item_count):
-		planet.shipyard_level += item_count
+	def effect(self, planet):
+		planet.shipyard_level ++
 
 #class ResearchFrigate(ResearchBase):
 
 #	def __init__(self, planet):
 #		super().__init__("Upgrade Fighters", 50 * planet.fighters_level)
 
-#	def effect(self, planet, item_count):
-#		planet.fighters_level += item_count
+#	def effect(self, planet):
+#		planet.fighters_level ++
