08405fff2eb7dad6bdcb8bd02340c90a2e687ebe
Bering <bering@ringlogic.com>
2019-01-12 22:04:52 -0500

    Optimized ScreenQuit

--- screen_quit.py
+++ screen_quit.py
@@ -5,6 +5,9 @@ class QuitScreen(ScreenBase):
 
 	def __init__(self, app):
 		super().__init__(app)
+		font = pygame.font.Font(None, 24)
+		self.text_surf = font.render("Are you sure?", True, (255,255,255))
+		self.text_rect = self.text_surf.get_rect()
 
 	def on_event(self, event):
 		if (event.type == pygame.KEYUP):
@@ -17,11 +20,7 @@ class QuitScreen(ScreenBase):
 		pass
 		
 	def render(self, world, surface):
-		font = pygame.font.Font(None, 24)
-		text_surf = font.render("Are you sure?", True, (255,255,255))
-		rect = text_surf.get_rect()
-		rect.center = surface.get_rect().center
-		surface.blit(text_surf, rect)
-
-		rect.inflate_ip(30, 30)
+		self.text_rect.center = surface.get_rect().center
+		surface.blit(self.text_surf, self.text_rect)
+		rect = self.text_rect.inflate(30, 30)
 		pygame.draw.rect(surface, (255,255,255), rect, 2)
