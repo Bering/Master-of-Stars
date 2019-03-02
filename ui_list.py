f341a732629bd1c16e7a009543f705d780fb1e2b
Bering <bering@ringlogic.com>
2019-03-02 08:53:14 -0500

    UI List mostly complete

--- ui_list.py
+++ ui_list.py
@@ -2,16 +2,19 @@ import os
 import pygame
 from text_renderer import TextRenderer
 
+default_color = (255, 255, 255)
+default_highlight_color = (48, 48, 48)
+
 class UIListItem:
 	def __init__(self, surface, obj, highlighted):
 		self.surface = surface
 		self.rect = self.surface.get_rect()
 		self.object = obj
-		self.highlighted = highlighted
+		self.is_highlighted = highlighted
 
 # TODO: add a center parameter? Then we could use either position or center to place the list
 class UIList:
-	def __init__(self, items, position):
+	def __init__(self, items, position, color=default_color, highlight_color=default_highlight_color):
 		filename = os.path.join("fonts", "OpenSansRegular.ttf")
 		font = pygame.font.Font(filename, 16)
 		text_renderer = TextRenderer(font)
@@ -23,9 +26,15 @@ class UIList:
 		width = 0
 		for item in items:
 			b = UIListItem(
-				text_renderer.render(item, True, (255,255,255)),
+				text_renderer.render(item, True, color),
 				item,
 				False
+			)
+			b.surface_highlighted = text_renderer.render(
+				item,
+				True,
+				color,
+				highlight_color
 			)
 			self.buttons.append(b)
 			width = max(width, b.rect.width)
@@ -33,43 +42,46 @@ class UIList:
 		padding = 16
 		for b in self.buttons:
 			text_surface = b.surface.copy()
+			text_surface_h = b.surface_highlighted.copy()
 			b.surface = pygame.Surface((width + padding, b.rect.height + padding))
+			b.surface_highlighted = b.surface.copy()
+			b.surface_highlighted.fill(highlight_color)
+			b.rect.move_ip(padding/2, padding/2)
 			b.surface.blit(text_surface, b.rect)
+			b.surface_highlighted.blit(text_surface_h, b.rect)
 			b.rect = b.surface.get_rect()
-			pygame.draw.rect(b.surface, (255,255,255), b.rect, 2)
+			pygame.draw.rect(b.surface, color, b.rect, 2)
+			pygame.draw.rect(b.surface_highlighted, color, b.rect, 2)
 
 		height = 0
 		for n in range(len(self.buttons)):
 			height += self.buttons[n].rect.height
-			if n == 0:
-				self.buttons[n].rect.center = position
-			else:
+			if n > 0:
 				self.buttons[n].rect.topleft = self.buttons[n-1].rect.bottomleft
+				self.buttons[n].rect.y -= 1
 
-		self.rect = pygame.Rect((0, 0), (width, height))
+		self.rect = pygame.Rect((0, 0), (width + padding, height - (len(self.buttons) - 1)))
 		self.rect.center = position
-
-#		self.surface = pygame.Surface((width + padding, height + 1))
-#		self.rect = self.surface.get_rect()
-#		self.rect.center = position
-#		pygame.draw.rect(self.surface, (255,0,0), self.rect, 2)
-
-#		for button in self.buttons:
-#			button.rect.move_ip(padding / 2, padding / 2)
-#			button.rect.width = width
-#			self.surface.blit(button.surface, button.rect)
-#			button.rect.inflate_ip(padding, padding)
-#			pygame.draw.rect(self.surface, (255,255,255), button.rect, 2)
-#			self.surface.blit(button.surface, button.rect)
 
 	def handle_click(self, event):
 		for n in range(len(self.buttons)):
 			if self.buttons[n].rect.collidepoint(event.pos):
-				self.selected_index = n
+				self.select(n)
 				return n
 		return None
 
+	def select(self, index):
+		self.selected_index = index
+		for n in range(len(self.buttons)):
+			self.buttons[n].is_highlighted = (index == n)
+
 	def render(self, surface):
+		delta_x = self.rect.topleft[0] - self.buttons[0].rect.x
+		delta_y = self.rect.topleft[1] - self.buttons[0].rect.y
 		for button in self.buttons:
-			surface.blit(button.surface, button.rect)
-		pygame.draw.rect(surface, (255,0,0), self.rect, 2)
+			if delta_x != 0 or delta_y != 0:
+				button.rect.move_ip(delta_x, delta_y)
+			if button.is_highlighted:
+				surface.blit(button.surface_highlighted, button.rect)
+			else:
+				surface.blit(button.surface, button.rect)
